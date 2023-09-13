8b52f8085f0b2f5deb3d03d8d8003afcadee9c73
John Aughey <jha@aughey.com>
2023-09-13 20:40:21 +0000

    working on new parser

--- rust_satellite/src/lib.rs
+++ rust_satellite/src/lib.rs
@@ -1,6 +1,13 @@
+use std::collections::HashMap;
+
 pub use anyhow::Result;
 use clap::Parser;
-use nom::{branch::alt, bytes::complete::tag, character::complete::multispace0, IResult};
+use nom::{
+    branch::alt,
+    bytes::complete::{tag, take, take_while1},
+    character::complete::multispace0,
+    IResult,
+};
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
@@ -17,6 +24,79 @@ pub struct Cli {
     pub port: u16,
 }
 
+// returns the next character, or the subsequent characters if the first is a backslash
+fn char_or_escaped_char(data: &str) -> IResult<&str, &str> {
+    let (data, maybe_backslash) = take(1usize)(data)?;
+
+    if data == "\\" {
+        let (data, escaped_char) = take(1usize)(data)?;
+        Ok((data, escaped_char))
+    } else {
+        Ok((data, maybe_backslash))
+    }
+}
+
+enum StringOrStr<'a> {
+    String(String),
+    Str(&'a str),
+}
+
+// parse a quoted string, with escaped characters
+fn quoted_string(data: &str) -> IResult<&str, StringOrStr> {
+    // initial quote
+    let (data, _) = tag("\"")(data)?;
+    // char_or_escaped_char will return the next value.  Accumulate this until
+    let mut head = data;
+    let mut accum = String::new();
+    loop {
+        let (data, value) = char_or_escaped_char(head)?;
+        head = data;
+        if value == "\"" {
+            break;
+        }
+        accum.push_str(value);
+    }
+
+    Ok((head, StringOrStr::String(accum)))
+}
+
+fn unquoted_string(data: &str) -> IResult<&str, StringOrStr> {
+    let (data, value) = nom::bytes::complete::take_while(|c: char| !c.is_whitespace())(data)?;
+    Ok((data, StringOrStr::Str(value)))
+}
+
+fn str_to_key_value(data: &str) -> IResult<&str, HashMap<String, StringOrStr>> {
+    let mut key_values = HashMap::new();
+
+    let mut head = data;
+    loop {
+        // Check for empty
+        if head.is_empty() {
+            break;
+        }
+        // using nom, trim whitesapce
+        let (data, _) = multispace0(head)?;
+        // Check again just in case trailing whitespace
+        if data.is_empty() {
+            head = data;
+            break;
+        }
+        // parse key, letters, numbers, underscores, dashes
+        let (data, key) = nom::bytes::complete::take_while(|c: char| {
+            c.is_ascii_alphanumeric() || c == '_' || c == '-'
+        })(data)?;
+        // parse =
+        let (data, _) = tag("=")(data)?;
+        // parse value, a quoted string or a non-quoted string with no whitespace
+        let (data, value) = alt((quoted_string, unquoted_string))(data)?;
+        // insert into map
+        key_values.insert(key.to_string(), value);
+        head = data;
+    }
+
+    Ok((head, key_values))
+}
+
 trait Parse<'a> {
     fn parse(data: &'a str) -> IResult<&str, Self>
     where
@@ -30,6 +110,7 @@ pub fn iresult_unwrap<D, R>(value: IResult<D, R>) -> Result<R> {
     }
 }
 
+#[derive(Debug, PartialEq, Eq)]
 pub enum Command<'a> {
     Pong,
     Begin(Versions<'a>),
@@ -59,17 +140,19 @@ impl<'a> Command<'a> {
     }
 }
 
+#[derive(PartialEq, Eq)]
 pub struct KeyState<'a> {
     pub device: &'a str,
     pub key: u8,
     pub button_type: &'a str,
     pub bitmap_base64: &'a str,
-    pub pressed: bool
+    pub pressed: bool,
 }
 impl KeyState<'_> {
     pub fn bitmap(&self) -> Result<Vec<u8>> {
         use base64::Engine as _;
-        let data = base64::engine::general_purpose::STANDARD_NO_PAD.decode(self.bitmap_base64.as_bytes())?;
+        let data = base64::engine::general_purpose::STANDARD_NO_PAD
+            .decode(self.bitmap_base64.as_bytes())?;
         Ok(data)
     }
 }
@@ -119,7 +202,7 @@ impl std::fmt::Debug for KeyState<'_> {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 pub struct Brightness<'a> {
     pub device: &'a str,
     pub brightness: u8,
@@ -140,7 +223,7 @@ impl<'a> Parse<'a> for Brightness<'a> {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 pub struct AddDevice<'a> {
     pub success: bool,
     pub device_id: &'a str,
@@ -166,7 +249,7 @@ impl<'a> Parse<'a> for AddDevice<'a> {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 pub struct Versions<'a> {
     pub companion_version: &'a str,
     pub api_version: &'a str,
@@ -192,6 +275,7 @@ impl<'a> Parse<'a> for Versions<'a> {
     }
 }
 
+#[derive(Debug, PartialEq, Eq)]
 pub struct DeviceMsg {
     pub device_id: String,
     pub product_name: String,
@@ -202,5 +286,71 @@ impl DeviceMsg {
     pub fn device_msg(&self) -> String {
         format!("DEVICEID={} PRODUCT_NAME=\"{}\" KEYS_TOTAL={}, KEYS_PER_ROW={} BITMAPS=120 COLORS=0 TEXT=0",
             self.device_id, self.product_name, self.keys_total, self.keys_per_row)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_pong_command() {
+        const DATA: &str = "PONG";
+        let command = Command::parse(DATA).unwrap();
+        assert_eq!(command, Command::Pong);
+    }
+
+    #[test]
+    fn test_begin() {
+        const DATA: &str = "BEGIN CompanionVersion=3.99.0+6259-develop-a48ec073 ApiVersion=1.5.1";
+        let command = Command::parse(DATA).unwrap();
+        assert_eq!(
+            command,
+            Command::Begin(Versions {
+                companion_version: "3.99.0+6259-develop-a48ec073",
+                api_version: "1.5.1"
+            })
+        );
+    }
+
+    #[test]
+    fn test_adddevice() {
+        const DATA: &str = "ADD-DEVICE OK DEVICEID=\"JohnAughey\"";
+        let command = Command::parse(DATA).unwrap();
+        assert_eq!(
+            command,
+            Command::AddDevice(AddDevice {
+                success: true,
+                device_id: "JohnAughey"
+            })
+        );
+    }
+
+    #[test]
+    fn test_keystate() {
+        const DATA: &str =
+            "KEY-STATE DEVICEID=JohnAughey KEY=14 TYPE=BUTTON  BITMAP=rawdata PRESSED={true,false}";
+        let command = Command::parse(DATA).unwrap();
+        assert_eq!(
+            command,
+            Command::KeyState(KeyState {
+                device: "JohnAughey",
+                key: 14,
+                button_type: "BUTTON",
+                bitmap_base64: "rawdata",
+                pressed: false
+            })
+        );
+    }
+
+    #[test]
+    fn test_keyvalue_parser() {
+        const DATA: &str =
+            "DEVICEID=JohnAughey KEY=14 TYPE=BUTTON  BITMAP=rawdata PRESSED={true,false}";
+        let (_, key_values) = str_to_key_value(DATA).unwrap();
+        let mut keys = key_values.keys().map(|s| s.to_owned()).collect::<Vec<_>>();
+        keys.sort();
+
+        assert_eq!(keys, vec!["BITMAP", "DEVICEID", "KEY", "PRESSED", "TYPE",]);
     }
 }
