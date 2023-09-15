28b1804e875148530b45f2f1034bc97d37875b0a
John Aughey <jha@aughey.com>
2023-09-15 15:34:10 +0000

    tracing

--- simple_deck/src/main.rs
+++ simple_deck/src/main.rs
@@ -1,6 +1,9 @@
 use streamdeck::StreamDeck;
 
 fn main() {
+    // enable tracing
+    tracing_subscriber::fmt::init();
+
      // Connect to device
      let mut deck =  StreamDeck::connect(0x0fd9, 0x0084, None).unwrap();
 
