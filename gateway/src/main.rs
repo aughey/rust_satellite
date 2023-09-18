1a8ad990304db56e24d8e8d038f48324a0502fb7
John Aughey <jha@aughey.com>
2023-09-18 19:57:14 +0000

    asdf

--- gateway/src/main.rs
+++ gateway/src/main.rs
@@ -192,7 +192,7 @@ where
                         let image = image.resize_exact(
                             image_format.size.0 as u32,
                             image_format.size.1 as u32,
-                            image::imageops::FilterType::Nearest,
+                            image::imageops::FilterType::Lanczos3,
                         );
                         // Convert the image into the EXACT format needed for the remote device
                         let image = elgato_streamdeck::images::convert_image(kind, image)?;
